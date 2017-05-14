62a2f2bff09285c08b10ca557db9ec8559c7c2a9
Silvertorch5 <silvertorch5@gmail.com>
2017-05-13 19:03:20 -0600

    Add Shapes Framework

---
+++ Graphics/Shape/Line.cs
@@ -0,0 +1,30 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+
+namespace RobustEngine.Graphics.Shape
+{
+    public class Line
+    {
+        private int VertexBufferID;
+        
+
+        public Line(int a, int b)
+        {
+            VertexBufferID = GL.GenBuffer();
+       //     GL.BindBuffer(ArrayBuffer.BufferTarget, VertexBufferID);
+
+
+            
+            
+
+
+        }
+            
+       
+    }
+}
