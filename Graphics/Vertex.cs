49130f5ae38ddb32131d20bbf72cf854b8ad870d
Silvertorch5 <silvertorch5@gmail.com>
2017-05-13 19:43:33 -0600

    Update Vertex

--- Graphics/Vertex.cs
+++ Graphics/Vertex.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace RobustEngine.Graphics
 {
-    class Vertex
+    public struct Vertex
     {
+        public float X, Y, Z;
+        public Vertex(float x, float y, float z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
     }
+ 
 }
