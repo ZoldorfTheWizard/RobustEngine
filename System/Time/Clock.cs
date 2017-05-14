26565d1f29fffccef1f93f193b8263f93232944d
Silvertorch5 <silvertorch5@gmail.com>
2017-05-13 19:11:33 -0600

    Adds Tick tock
    goes the clock

---
+++ System/Time/Clock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobustEngine.System.Time
+{
+    public class Clock
+    {
+
+        public DateTime StartTime;
+        public DateTime StopTime;
+
+        public Clock()
+        {
+                     
+        }
+
+        public void Reset()
+        {
+            StartTime = new DateTime();
+            StopTime  = new DateTime();  
+        }
+
+        public void Start()
+        {
+            StartTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            StopTime = DateTime.Now;
+        }
+
+        public TimeSpan GetTime()
+        {
+            return StartTime.Subtract(StopTime); ;
+        }
+
+        public TimeSpan GetElapsed()
+        {
+            return -StartTime.Subtract(DateTime.Now);
+        
+        }
+
+    }
+}
