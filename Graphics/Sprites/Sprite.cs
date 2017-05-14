6d1f91ae3c4fe9a2a36d935af732d6cc6e9b5661
Silvertorch5 <silvertorch5@gmail.com>
2017-05-13 19:04:22 -0600

    Add Sprites

---
+++ Graphics/Sprites/Sprite.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+using OpenTK;
+
+namespace RobustEngine.Graphics.Sprites
+{
+    public class Sprite
+    {
+        public string ID;               
+
+    
+        //GL VBO'S
+        public int VertexBuffer;
+        public int TextureBuffer;
+        public int ColorBuffer;
+
+        //Accessors TODO
+        public double Rotation;
+        public int Width;
+        public int Height;
+        public Rectangle SpriteAABB;
+        public Texture2D Texture;
+        public Vector2 Scale;
+        public Vector2 Position;
+
+        public Sprite(string id, Texture2D texture)
+        {
+            ID = id;
+            Texture = texture;
+            Setup();
+        }   
+
+        private void Setup()
+        {          
+            SpriteAABB = Texture.TextureAABB;
+            Width      = SpriteAABB.Width;
+            Height     = SpriteAABB.Height;
+            Rotation   = 0.0;
+            Scale      = Vector2.One;
+            Position   = Vector2.Zero;
+        }
+        
+        public void SetRotation(double newRotation)
+        {
+            Rotation = newRotation;
+        }
+
+        public void SetPosition(Vector2 newPosition)
+        {
+            Position = newPosition;
+        }
+
+        public void SetScale(Vector2 newScale)
+        {
+            Scale = newScale;
+        }        
+
+        public void Blit()
+        {
+           
+        }
+        
+
+    }
+}
