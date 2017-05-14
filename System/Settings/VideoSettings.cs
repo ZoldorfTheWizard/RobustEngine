632fce5fe082c02b0661b368adc6d399a04ed611
Silvertorch5 <silvertorch5@gmail.com>
2017-05-13 19:11:12 -0600

    Adds VideoSettings

---
+++ System/Settings/VideoSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using System.IO;
+
+namespace RobustEngine.System.Settings
+{
+
+    [Serializable]
+    public class VideoSettings
+    {
+        public VSyncMode    VSync;
+        public WindowBorder Border;
+        public WindowIcon   Icon;
+        public Size         Size;
+        public bool         Fullscreen;
+        public int          Antialiasing;
+        public int          Anistrophic;
+
+
+        /// <summary>
+        /// Default Constructor. Builds a 600x800 window.
+        /// </summary>
+        public VideoSettings()
+        {
+            VSync = VSyncMode.Off;
+            Size = new Size(800,800);
+            Fullscreen = false;
+        }
+
+        public VideoSettings Load(string Pathtosettings)
+        {
+            return null; // Todo
+        }      
+
+
+    }
+
+
+}
