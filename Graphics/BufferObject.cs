7e4a21458eb9a78c527689bc78a98a20a2f7eabc
Silvertorch5 <silvertorch5@gmail.com>
2017-07-16 19:20:48 -0600

    BufferObject Changes
    Texcoords have been moved into Vertex Struct.
    Remove float test array.
    
    Add a update method to update VBO Contents.

--- Graphics/BufferObject.cs
+++ Graphics/BufferObject.cs
@@ -3,6 +3,7 @@ using System;
 using System.Drawing;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using System.Runtime.InteropServices;
 
 namespace RobustEngine.Graphics
 {
@@ -15,25 +16,32 @@ namespace RobustEngine.Graphics
         private int ColorBOID;        // Color Buffer Obj ID
 
         private Vertex[] Verticies;
-        private Vertex[] TexCoord;
-        private float[] Indicies;
+        private int[] Indicies;
 
+        /// <summary>
+        /// Creates a new buffer object with defaults.
+        /// </summary>
         public BufferObject()
         {
             Verticies = new Vertex[]
             {
-                Vertex.Zero,
-                Vertex.UnitX,
-                Vertex.One,
-                Vertex.UnitY
+                Vertex.Zero  * .5f,
+                Vertex.UnitX * .5f,
+                Vertex.One   * .5f,
+                Vertex.UnitY * .5f             
             };
 
-            Create(); 
+            Create();
         }
-        
+
+
+        /// <summary>
+        /// Creates a buffer object with specified verts.
+        /// </summary>
+        /// <param name="Verts"></param>
         public BufferObject(Vertex[] Verts)
         {
-            Verticies = Verts;    
+            Verticies = Verts;
             Create();
 
         }
@@ -44,58 +52,72 @@ namespace RobustEngine.Graphics
         private void Create()
         {
 
-            // 0,1 1,1  3 2
-            // 0,0 0,1  0 1
-            TexCoord = new Vertex[]
+            Verticies[0].Tx = 0f;
+            Verticies[0].Ty = 1f;
+
+            Verticies[1].Tx = 1f;
+            Verticies[1].Ty = 1f;
+
+            Verticies[2].Tx = 1f;
+            Verticies[2].Ty = 0f;
+
+            Verticies[3].Tx = 0f;
+            Verticies[3].Ty = 0f;
+           
+            Verticies[1].R = 1f;
+            Verticies[2].G = 1f;
+            Verticies[3].B = 1f;
+
+            Indicies = new int[]
             {
-                Vertex.Zero,
-                Vertex.UnitX,
-                Vertex.One,
-                Vertex.UnitY,
+                  0, 1, 2, // First Triangle
+                  2, 3, 0  // Second Triangle
             };
 
-            //float[] vertices =
-            //{
-            //    -0.5f, -0.5f, 0.0f,
-            //     0.5f, -0.5f, 0.0f,
-            //     0.0f,  0.5f, 0.0f
-            //};
+            VertexArrayID = GL.GenVertexArray(); // Opengl Please give us a empty State
+            GL.BindVertexArray(VertexArrayID);   // Opengl Please use and write everything to this new empty state now thx            
 
-            // 0 1
-            // 2 3  
-            Indicies = new float[]
-            {
-                0, 1, 2,
-                2, 3, 1 
-            };
-
-            
-            VertexArrayID = GL.GenVertexArray();
-            GL.BindVertexArray(VertexArrayID); // Buffers now write to this VAO.
-            
             //Generate Buffers
-            VertexBufferID = GL.GenBuffer();
-            IndexBufferID = GL.GenBuffer();
-            ColorBOID = GL.GenBuffer();
+            VertexBufferID = GL.GenBuffer(); // Hey Opengl we want a empty VBO to use for data pls generate thx
+            IndexBufferID = GL.GenBuffer();  // Hey Opengl we want a empty VBO to use for indexing pls generate thx
+            ColorBOID = GL.GenBuffer();      // Hey Opengl we want a empty VBO to use for colors pls generate thx
+            
+            // VBO
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferID); 
+            GL.BufferData(BufferTarget.ArrayBuffer, Verticies.Length * Vertex.Stride, Verticies, BufferUsageHint.DynamicDraw);
+                    
+            // Vertex Data
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, Vertex.Stride, 0);
+            GL.EnableVertexAttribArray(0); // Layout 1 Position Data
 
-            //Bind Buffer to Gen Buffer.
-            //Describe Buffer.
-            //Bind 0 to go back to default buffer
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferID);
-            GL.BufferData<Vertex>(BufferTarget.ArrayBuffer, (IntPtr)(Verticies.Length * Vertex.Stride), Verticies, BufferUsageHint.DynamicDraw);
-            GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, Vertex.Stride, 0);
-            GL.EnableVertexAttribArray(0);
+            // Color Data
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, Vertex.Stride, 12); 
+            GL.EnableVertexAttribArray(1); // Layout 2 Color Data
+            
+            // Normal Data
+            GL.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, false, Vertex.Stride, 24);
+            GL.EnableVertexAttribArray(2); // Layout 2 Color
+
+            // TextureUVCoords
+            GL.VertexAttribPointer(3, 2, VertexAttribPointerType.Float, false, Vertex.Stride, 36); 
+            GL.EnableVertexAttribArray(3); // Layout 3 TexCoord
+
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
 
-          //  GL.BindVertexArray(0);
-
-            //Indicies
+            //INDEX DATA
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, IndexBufferID);
             GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(Indicies.Length * sizeof(float)), Indicies, BufferUsageHint.DynamicDraw);
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
 
             GL.BindVertexArray(0);
 
+        }
+
+        public void Update()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferID);
+            GL.BufferData(BufferTarget.ArrayBuffer, Verticies.Length * Vertex.Stride, Verticies, BufferUsageHint.DynamicDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
         }
 
 
@@ -122,5 +144,21 @@ namespace RobustEngine.Graphics
         }
 
 
+        public void move(float mov)
+        {
+            for (int i = 0; i < 4; i++)
+            { 
+                //Verticies[i].Y = Verticies[i].Y + mov;
+               // Verticies[i].Z = Verticies[i].Z + mov;
+            }
+        }
+
+    }
+
+    public struct test
+    {
+        public float x;
+        public float y;
+        public float z;
     }
 }
