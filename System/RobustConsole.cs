024d2d42cb02ae186a4d9a9fc1afc51289fb184d
Silver <silvertorch5@gmail.com>
2017-07-23 02:33:02 -0600

    Update Robust Console

--- System/RobustConsole.cs
+++ System/RobustConsole.cs
@@ -9,13 +9,17 @@ namespace RobustEngine.System
 {
     public static class RobustConsole
     {
-        private static DateTime Time;
         private static LogLevel ConsoleLL;
 
         static RobustConsole()
         {
-            Time = new DateTime();
-        }
+            
+		}
+
+		public static void ClearConsole()
+		{
+			Console.Clear();
+		}
 
         public static void SetLogLevel(LogLevel LL)
         {
@@ -31,14 +35,19 @@ namespace RobustEngine.System
 
             var CallingClass ="";
 
-            if (Class.GetType() == typeof(string))
-            {
-                CallingClass = (string)Class;
-}
-            else
-            {
-                CallingClass = Class.GetType().Name;
-            }
+			if (Class.GetType() == typeof(string))
+			{
+				CallingClass = (string)Class;
+			}
+			else
+			{
+				CallingClass = Class.GetType().Name;
+			}
+
+			if (Reason.Length == 0)
+				return; 
+
+			Reason = Reason.Replace(". ", ".\n");
 
             switch (LL)
             {
@@ -55,37 +64,37 @@ namespace RobustEngine.System
         private static void WriteVerbose(string whonnock, string whynnock)
         {
             Sysconsole.ForegroundColor = ConsoleColor.DarkGray;
-            Sysconsole.Out.Write("|" + DateTime.Now.ToShortTimeString() + "|[" + whonnock + "] VERBOSE: " + whynnock + "\n");
+            Sysconsole.Out.Write(DateTime.Now.ToShortTimeString() + "[" + whonnock + "] VERBOSE: " + whynnock + "\n");
         }
 
         private static void WriteDebug(string whonnock , string whynnock)
         {          
             Sysconsole.ForegroundColor = ConsoleColor.Gray;
-            Sysconsole.Out.Write("|" + DateTime.Now.ToShortTimeString() + "|[" + whonnock+ "] DEBUG: " + whynnock + "\n");
+            Sysconsole.Out.Write(DateTime.Now.ToShortTimeString() + "[" + whonnock+ "] DEBUG: " + whynnock + "\n");
         }
 
         private static void WriteInfo(string whonnock, string whynnock)
         {
             Sysconsole.ForegroundColor = ConsoleColor.White;
-            Sysconsole.Out.Write("|" + DateTime.Now.ToShortTimeString() + "|["+ whonnock+ "] INFO: " + whynnock + "\n");
+            Sysconsole.Out.Write(DateTime.Now.ToShortTimeString() + "["+ whonnock+ "] INFO: " + whynnock + "\n");
         }
 
         private static void WriteWarning(string whonnock, string whynnock)
         {
             Sysconsole.ForegroundColor = ConsoleColor.DarkYellow;
-            Sysconsole.Out.Write("|" + DateTime.Now.ToShortTimeString() + "|[" + whonnock + "] WARNING: " + whynnock + "\n");
+            Sysconsole.Out.Write(DateTime.Now.ToShortTimeString() + "[" + whonnock + "] WARNING: " + whynnock + "\n");
         }
 
         private static void WriteCritical(string whonnock, string whynnock)
         {
             Sysconsole.ForegroundColor = ConsoleColor.DarkRed;
-            Sysconsole.Out.Write("|" + DateTime.Now.ToShortTimeString() + "|[" + whonnock+ "] CRITICAL: " + whynnock + "\n");
+            Sysconsole.Out.Write( DateTime.Now.ToShortTimeString() + "[" + whonnock+ "] CRITICAL: " + whynnock + "\n");
         }
 
         private static void WriteFatal(string whonnock, string whynnock)
         {
             Sysconsole.ForegroundColor = ConsoleColor.Red;
-            Sysconsole.Out.Write("|" + DateTime.Now.ToShortTimeString() + "|[" + whonnock+ "] FATAL: " + whynnock + "\n");
+            Sysconsole.Out.Write(DateTime.Now.ToShortTimeString() + "[" + whonnock+ "] FATAL: " + whynnock + "\n");
         }
         #endregion
 
