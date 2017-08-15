d107101405676b4183318991fd35a3611a6606b5
Silver <silvertorch5@gmail.com>
2017-08-14 21:42:07 -0600

    Move Transform Methods to new Interface

--- Graphics/IRenderable2D.cs
+++ Graphics/IRenderable2D.cs
@@ -10,9 +10,6 @@ namespace RobustEngine.Graphics
 {
     public interface IRenderable2D
     {
-        void SetScale(Vector2 Scale);
-        void SetRotation(float Rotation);
-        void SetPosition(Vector2 Position);
         void Draw();
     }
 }
