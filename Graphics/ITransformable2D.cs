fe5b47e3809ff9981a1a4a2746a32996ddd07fe7
Silver <silvertorch5@gmail.com>
2017-08-14 21:42:20 -0600

    Add ITransform2D

---
+++ Graphics/ITransformable2D.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK;
+
+namespace RobustEngine.Graphics
+{
+    interface ITransformable2D
+    {
+        void SetScale(Vector2 Scale);
+        void SetRotation(float Rotation);
+        void SetPosition(Vector2 Position);
+
+        void Scale(Vector2 Scale);
+        void Rotate(float Rotation);
+        void Move(Vector2 Position);
+    }
+}
