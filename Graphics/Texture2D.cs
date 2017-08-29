a22259dd7ad38b9a719fa6bc02e7633f6efb9eb8
Silver <silvertorch5@gmail.com>
2017-08-28 19:11:00 -0600

    Add TextureUnit Functionality, Remove Blend Enums

--- Graphics/Texture2D.cs
+++ Graphics/Texture2D.cs
@@ -13,9 +13,9 @@ namespace RobustEngine.Graphics
         public int ID;
         public Rectangle AABB;
 
+        private int TextureSlot;
         private Bitmap Bitmap;
         private BitmapData BitmapData;
-
         private Color[,] PixelData;
 
         /// <summary>
@@ -29,10 +29,14 @@ namespace RobustEngine.Graphics
         }
 
         /// <summary>
-        /// Bind Texture
+        /// Bind Texture.   
         /// </summary>
-        public void Bind()
+        /// <returns>The bind.</returns>
+        /// <param name="TextureUnit">Specify which TextureUnit you want to bind to. 0-16.</param>
+        public void Bind(int TexUnit = 0)
         {
+            TextureSlot = TexUnit;
+            GL.ActiveTexture(TextureUnit.Texture0 + TextureSlot);
             GL.BindTexture(TextureTarget.Texture2D, ID);
         }
 
@@ -41,6 +45,7 @@ namespace RobustEngine.Graphics
         /// </summary>
         public void Unbind()
         {
+            GL.ActiveTexture(TextureUnit.Texture0 + TextureSlot);
             GL.BindTexture(TextureTarget.Texture2D, 0);
         }
 
@@ -94,7 +99,7 @@ namespace RobustEngine.Graphics
             //TODO Mipmap + Bump map here maybe?
 
             Unbind();
-
+            Bitmap.Dispose();
         }
 
         public bool IsOpaque(int x, int y)
@@ -105,59 +110,4 @@ namespace RobustEngine.Graphics
 
     }
 
-
-    public enum BlendMode
-    {
-        /// <summary>No blending.</summary>
-        None = 0,
-        /// <summary>Modulated blending.</summary>
-        Modulated = 1,
-        /// <summary>Additive blending.</summary>
-        Additive = 2,
-        /// <summary>Inverse modulated blending.</summary>
-        ModulatedInverse = 4,
-        /// <summary>Color blending.</summary>
-        Color = 8,
-        /// <summary>Additive color.</summary>
-        ColorAdditive = 16,
-        /// <summary>Use premultiplied.</summary>
-        PreMultiplied = 32,
-        /// <summary>Invert.</summary>
-        Inverted = 64
-    }
-
-    /// <summary>
-    /// Enumeration for alpha blending operations.
-    /// </summary>
-    public enum AlphaBlendOperation
-    {
-        /// <summary>Blend factor of 0,0,0.</summary>
-        Zero = 0,
-        /// <summary>Blend factor is 1,1,1.</summary>
-        One = 1,
-        /// <summary>Blend factor is Rs', Gs', Bs', As.</summary>
-        SourceColor = 2,
-        /// <summary>Blend factor is As', As', As', As.</summary>
-        SourceAlpha = 3,
-        /// <summary>Blend factor is 1-Rs', 1-Gs', 1-Bs', 1-As.</summary>
-        InverseSourceColor = 4,
-        /// <summary>Blend factor is 1-As', 1-As', 1-As', 1-As.</summary>
-        InverseSourceAlpha = 5,
-        /// <summary>Blend factor is Rd', Gd', Bd', Ad.</summary>
-        DestinationColor = 6,
-        /// <summary>Blend factor is Ad', Ad', Ad', Ad.</summary>
-        DestinationAlpha = 7,
-        /// <summary>Blend factor is 1-Rd', 1-Gd', 1-Bd', 1-Ad.</summary>
-        InverseDestinationColor = 8,
-        /// <summary>Blend factor is 1-Ad', 1-Ad', 1-Ad', 1-Ad.</summary>
-        InverseDestinationAlpha = 9,
-        /// <summary>Blend factor is f,f,f,1 where f = min(A, 1-Ad)</summary>
-        SourceAlphaSaturation = 10,
-        /// <summary>Source blend factor is 1-As', 1-As', 1-As', 1-As and destination is As', As', As', As.  Overrides the blend destination, and is only valid if the SourceBlend state is true.</summary>
-        BothInverseSourceAlpha = 11,
-        /// <summary>Constant color blend factor.  Only valid if the driver SupportBlendingFactor is true.</summary>
-        BlendFactor = 12,
-        /// <summary>Inverted constant color blend factor.  Only valid if the driver SupportBlendingFactor capability is true.</summary>
-        InverseBlendFactor = 13
-    }
 }
