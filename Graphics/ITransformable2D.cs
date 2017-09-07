c7b649d9cb97729f238621b2a5ad6d23db4252f5
Silver <silvertorch5@gmail.com>
2017-09-06 20:30:55 -0600

    Fix ITransformable.cs

--- Graphics/ITransformable2D.cs
+++ Graphics/ITransformable2D.cs
@@ -4,12 +4,9 @@ namespace RobustEngine.Graphics
 {
     interface ITransformable2D
     {
-        void SetScale(Vector2 Scale);
-        void SetRotation(float Rotation);
-        void SetPosition(Vector2 Position);
-
-        void Scale(Vector2 Scale);
-        void Rotate(float Rotation);
-        void Move(Vector2 Position);
+        void SetOrigin(Vector2 origin);
+        void SetScale(Vector2 scale);
+        void SetRotation(float rotate, Axis axis);
+        void SetPosition(Vector2 newpos);
     }
 }
